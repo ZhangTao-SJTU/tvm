124df0ee07d9545507f8e770cb8dee3298140c71
shabeeb <shabeeb.ameen@gmail.com>
2025-10-05 19:24:54 -0400

    minor scripts update

--- ensemble_generator.py
+++ ensemble_generator.py
@@ -7,16 +7,19 @@ os.makedirs(dir, exist_ok= True)
 for run in range(100):
     run_dir = dir+"{:03d}/".format(run)
     os.makedirs("{}".format(run_dir), exist_ok= True)
-    if os.path.isfile("{}minimized.txt".format(run_dir)):
-        continue
-    print(run_dir)
+    # if os.path.isfile("{}minimized.txt".format(run_dir)):
+    #     continue
+    print("\n\n     {}\n\n".format(run_dir))
     os.system("cp conf_l_4  {}conf".format(run_dir))
     minimization_found = False
     while not minimization_found:
         try:
-            os.system("cd {} && python /home/shabeeb/Projects/tvm-fire/scripts/tvm/main.py conf".format(run_dir))
-            minimize_config_in_dir(run_dir,"/home/shabeeb/Projects/tvm-fire/build/")
+            # os.system("cd {} && python /home/shabeeb/Projects/tvm-fire/scripts/tvm/main.py conf".format(run_dir))
+            # minimize_config_in_dir(run_dir, input_file = "sample.topo", cpp_executable_dir = "/home/shabeeb/Projects/tvm-fire/build/")
+            minimize_config_in_dir(run_dir, input_file = "minimized.txt", cpp_executable_dir = "/home/shabeeb/Projects/tvm-fire/build/")
             minimization_found = True
         except:
             print("Minimization failed, retrying with a new tesellation")
+            os.system("cd {} && python /home/shabeeb/Projects/tvm-fire/scripts/tvm/main.py conf".format(run_dir))
+            minimize_config_in_dir(run_dir, input_file = "sample.topo", cpp_executable_dir = "/home/shabeeb/Projects/tvm-fire/build/")
             continue
--- scripts/minimize_config.py
+++ scripts/minimize_config.py
@@ -1,8 +1,8 @@
 from toolbox.periodic import PeriodicTissue
 from toolbox.minimization import FIREminimization
 import sys
-def minimize_config_in_dir(dir,cpp_executable_dir = "/home/shabeeb/Projects/tvm-fire/build/"):
-    tissue = PeriodicTissue.from_config(dir,"sample.topo")
+def minimize_config_in_dir(dir, input_file = "sample.topo", cpp_executable_dir = "/home/shabeeb/Projects/tvm-fire/build/"):
+    tissue = PeriodicTissue.from_config(dir,input_file)
     minimizer = FIREminimization.periodic_tissue(tissue)
     minimizer.set_cpp_executable_dir(cpp_executable_dir)
     minimizer.minimize_config()
