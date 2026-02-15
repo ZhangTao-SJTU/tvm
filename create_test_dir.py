6928bee454eb5b22b836e3a21e4621cd052c4b7b
shabeeb <shabeeb.ameen@gmail.com>
2026-02-15 17:41:00 -0500

    testing code

--- create_test_dir.py
+++ create_test_dir.py
@@ -97,9 +97,14 @@ def create_test_directory_spheroid_decrease():
             sample = PeriodicTissue.from_config(test_dir, "minimized.txt")
             Patterns.find_target_cells_in_spheroid(sample, n_spheroid = n_spheroid, n_cells = 1)
 def create_test_directory_multiple_patterns():
-    test_dir = "tests/1_2_cells_periodic/"
-    n_cells = 3
-    subpattern_n_cells =[1,2]
+
+
+    subpattern_n_cells =[1,1,2]
+    n_cells = sum(subpattern_n_cells)
+    test_dir = "tests/kv_10_l_4_p"
+    for p in subpattern_n_cells:
+        test_dir+="_{}".format(p)
+    test_dir+="/"
     tolerance = 1e-6
     max_iters = 10000
     clear_interval = 100
---
+++ single_cell_breakdown.sh
@@ -0,0 +1,3 @@
+python multiple_patterns.py tests/kv_10_l_4_p_1_1/
+python multiple_patterns.py tests/kv_10_l_4_p_1_1_1/
+python multiple_patterns.py tests/kv_10_l_4_p_1_1_1_1/
